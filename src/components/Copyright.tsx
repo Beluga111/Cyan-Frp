e53fbcdb2c7c825b9114ddd08a2d495a31b3320f
Beluga111 <101449376+Beluga111@users.noreply.github.com>
2024-05-17 17:51:09 +0800

    Update Copyright.tsx

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,13 +2,5 @@ import { Link as MuiLink, Typography } from '@mui/material';
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <MuiLink color="inherit" href="https://www.aehxy.com/">
-      Aehxy
-      </MuiLink>{' '}
-      2021-{new Date().getFullYear()}.
-    <br />
-      <br />
   );
 }
